87f415c7c475600c4bd05e69dd61f2d15e3d5c7f
andreihz <andrei.hutuca@gmail.com>
2017-10-20 13:36:55 +0300

    Implemented a simple OpenCV example
    Implemented a simple program that creates a bitmap and displays it, to
    test if OpenCV is properly working.

--- OpenCVDemoAttempt2/app/src/main/java/com/example/opencvdemoattempt2/MainActivity.java
+++ OpenCVDemoAttempt2/app/src/main/java/com/example/opencvdemoattempt2/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.opencvdemoattempt2;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
 
 public class MainActivity extends AppCompatActivity {
-
     private static final String TAG = "MainActivity";
 
     static {
-        if(!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Cacat spurcat");
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV initialization FAILED!");
         } else {
-            Log.d(TAG, "cacat facut haur");
+            Log.d(TAG, "OpenCV initialization SUCCESS!");
         }
     }
 
@@ -22,5 +33,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            if (status == LoaderCallbackInterface.SUCCESS ) {
+                // now we can call opencv code !
+                helloworld();
+            } else {
+                super.onManagerConnected(status);
+            }
+        }
+    };
+
+    @Override
+    public void onResume() {;
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_5,this, mLoaderCallback);
+        // you may be tempted, to do something here, but it's *async*, and may take some time,
+        // so any opencv call here will lead to unresolved native errors.
+    }
+
+    public void helloworld() {
+        // make a mat and draw something
+        Mat m = Mat.zeros(100,400, CvType.CV_8UC3);
+        Imgproc.putText(m, "hi there ;)", new Point(30,80), Core.FONT_HERSHEY_SCRIPT_SIMPLEX, 2.2, new Scalar(200,200,0), 2);
+
+        // convert to bitmap:
+        Bitmap bm = Bitmap.createBitmap(m.cols(), m.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(m, bm);
+        // find the imageview and draw it!
+        ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        iv.setImageBitmap(bm);
     }
 }
